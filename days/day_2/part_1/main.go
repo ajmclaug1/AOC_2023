42dcaeb64300a4de89c71873ce0b6049a3f31466
Anthony James McLaughlin <ajmclaug1@gmail.com>
2023-12-04 10:17:33 +0000

    updated day2 part 1

--- days/day_2/part_1/main.go
+++ days/day_2/part_1/main.go
@@ -4,15 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	colours_dict := map[string]string{"red": "0", "blue": "0", "green": "0"}
-	user_input := os.Args
-	input := colours_dict
-	input["red"] = user_input[1]
-	input["green"] = user_input[2]
-	input["blue"] = user_input[3]
+	colours_dict := map[string]int{"red": 0, "blue": 0, "green": 0}
+	//user_input := os.Args
+	var input = colours_dict
+	input["red"] = 3
+	input["green"] = 3
+	input["blue"] = 3
 
 	readFile, err := os.Open("data_example.txt")
 
@@ -22,8 +23,20 @@ func main() {
 	fileScanner := bufio.NewScanner(readFile)
 
 	fileScanner.Split(bufio.ScanLines)
-
+	//listofgames := []string{}
 	for fileScanner.Scan() {
-		fmt.Print(fileScanner.Text())
+		var new_dict = colours_dict
+		game := strings.Split(fileScanner.Text(), ":")
+		game[1] = strings.ReplaceAll(game[1], ";", ",")
+		game_list := strings.Split(strings.TrimSpace(game[1]), ",") 
+		for _, v := range game_list{
+			fmt.Print(v)
+			dict_list := strings.Split(v, " ")
+			new_dict[dict_list[0]] += 3
+
+		}
+		//listofgames = append(listofgames, game[1])
+		fmt.Println(new_dict)
+
 	}
 }
