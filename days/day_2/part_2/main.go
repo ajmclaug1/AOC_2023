31bf5a5b41f55e9d8c079774cd52ee7afb7f0a3f
Anthony James McLaughlin <ajmclaug1@gmail.com>
2023-12-05 08:05:10 +0000

    day 2 solution

--- days/day_2/part_2/main.go
+++ days/day_2/part_2/main.go
@@ -9,15 +9,8 @@ import (
 )
 
 func main() {
-	user_input := os.Args
-	input := map[string]int{"red": 0, "blue": 0, "green": 0}
-	red, _ := strconv.Atoi(user_input[1])
-	green, _ := strconv.Atoi(user_input[2])
-	blue, _ := strconv.Atoi(user_input[3])
-	input["red"] = red
-	input["green"] = green
-	input["blue"] = blue
-	running_total := 0
+
+	total := 0
 
 	readFile, err := os.Open("data.txt")
 
@@ -31,29 +24,25 @@ func main() {
 	for fileScanner.Scan() {
 		game := strings.Split(fileScanner.Text(), ":")
 		edit_draws := strings.ReplaceAll(game[1], ";", ",")
-
-		possible := find_impossible(input, edit_draws)
-		if possible {
-			number_to_add, _ := strconv.Atoi(strings.ReplaceAll(game[0], "Game ", ""))
-			running_total += number_to_add
-		}
+		total += find_min_cubes(edit_draws)
 
 	}
-	fmt.Println(running_total)
+	fmt.Println(total)
 
 }
 
-func find_impossible(input map[string]int, draws string) bool {
+func find_min_cubes(draws string) int {
+	input := map[string]int{"red": 0, "blue": 0, "green": 0}
 	draws_list := strings.Split(draws, ",")
 	for _, v := range draws_list {
 		draw_value := strings.Trim(v, " ")
 		draw_value_list := strings.Split(draw_value, " ")
 		no_of_dice, _ := strconv.Atoi(draw_value_list[0])
 		if input[draw_value_list[1]] < no_of_dice {
-			return false
+			input[draw_value_list[1]] = no_of_dice
 		}
 	}
 
-	return true
+	return input["red"] * input["blue"] * input["green"]
 
 }
